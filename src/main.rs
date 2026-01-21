0e51030507514ea021a38ccab7292a3fe5db1465
Bèr Kessels <ber@berk.es>
2026-01-21 11:12:44 +0100

    refactor: Move tracing into own subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
 use axum::{routing::get, Router};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+mod logging;
 
 mod config;
 use config::Config;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "credential_service=debug,tower_http=debug".into()),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
+    logging::tracing_init();
+
     tracing::info!("Starting Credential Service");
 
     let config = Config::from_env()?;
